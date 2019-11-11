d195f5df8c16cc59a4311617cf4c5040959d80c1
barakman <barakman@yahoo.com>
2019-11-11 15:02:56 +0200

    Cosmetic.

--- docify.js
+++ docify.js
@@ -2,9 +2,9 @@ const NODE_DIR     = "node_modules";
 const INPUT_DIR    = "node_modules/bancor-contracts/solidity/contracts";
 const CONFIG_DIR   = "docify";
 const OUTPUT_DIR   = "docify/contracts";
-const EXCLUDE_FILE = "docify/exclude.txt";
 const README_FILE  = "docify/README.md";
 const SUMMARY_FILE = "docify/SUMMARY.md";
+const EXCLUDE_FILE = "docify/exclude.txt";
 
 const fs        = require("fs");
 const path      = require("path");
