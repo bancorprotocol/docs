a790c5c52acbb3b789135a888e04a9dd5f50aea0
yudilevi <yudi@bancor.network>
2020-04-26 06:30:21 +0300

    added sdk docs

--- generate-sdk-docs.js
+++ generate-sdk-docs.js
@@ -1,38 +1,30 @@
-require("download-git-repo")("bancorprotocol/contracts", "node_modules/bancor-contracts", function (error) {
+require("download-git-repo")("bancorprotocol/bancor-sdk#redesign_and_cleanups", "node_modules/bancor-sdk", function (error) {
     if (error)
         throw error;
 
-    const fs        = require("fs");
     const spawnSync = require("child_process").spawnSync;
 
     const args = [
-        "node_modules/solidity-docgen/dist/cli.js",
-        "--input=node_modules/bancor-contracts/solidity/contracts",
-        "--output=api-reference/ethereum-smart-contracts",
-        "--templates=config/ethereum-smart-contracts",
-        "--solc-module=node_modules/truffle/node_modules/solc",
-        "--solc-settings=" + JSON.stringify({optimizer: {enabled: true, runs: 200}}),
-        "--contract-pages"
+        "--inputFiles", "node_modules/bancor-sdk/src/index.ts",
+        "--inputFiles", "node_modules/bancor-sdk/src/conversion_paths.ts",
+        "--inputFiles", "node_modules/bancor-sdk/src/history.ts",
+        "--inputFiles", "node_modules/bancor-sdk/src/pricing.ts",
+        "--inputFiles", "node_modules/bancor-sdk/src/utils.ts",
+        "--inputFiles", "node_modules/bancor-sdk/src/types.ts",
+        "--exclude", "**/blockchains/**/*.ts",
+        "--exclude", "**/core.ts",
+        "--exclude", "**/helpers.ts",
+        "--exclude", "**/sdk_module.ts",
+        "--excludeExternals",
+        "--excludeNotExported",
+        "--excludePrivate",
+        "--excludeProtected",
+        "--mode", "modules",
+        "--name", "Bancor SDK",
+        "--hideGenerator",
+        "--out", "sdk/sdk-api-reference"
     ];
 
-    const result = spawnSync("node", args, {stdio: ["inherit", "inherit", "pipe"]});
-    if (result.stderr.length > 0)
-        throw new Error(result.stderr);
-
-    function fix(pathName) {
-        if (fs.lstatSync(pathName).isDirectory()) {
-            for (const fileName of fs.readdirSync(pathName))
-                fix(pathName + "/" + fileName);
-        }
-        else if (pathName.endsWith(".md")) {
-            const lines = fs.readFileSync(pathName, {encoding: "utf8"}).split("\r").join("").split("\n");
-            fs.writeFileSync(pathName, lines.filter(line => line.trim().length > 0).join("\n\n") + "\n");
-        }
-    }
-
-    fix("api-reference/ethereum-smart-contracts");
-
-    fs.copyFileSync("node_modules/bancor-contracts/README.md", "api-reference/ethereum-smart-contracts/README.md");
-    fs.copyFileSync("node_modules/bancor-contracts/solidity/utils/README.md", "api-reference/ethereum-smart-contracts/solidity/utils/README.md");
-    fs.copyFileSync("node_modules/bancor-contracts/solidity/python/README.md", "api-reference/ethereum-smart-contracts/solidity/python/README.md");
+    const res = spawnSync("node_modules/typedoc/bin/typedoc", args);
+    console.log(String(res.stdout));
 });
