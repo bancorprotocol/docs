c93eb0296274e695ebfc3e9b6f7eae164a5fcb32
Barak Manos <barakman@yahoo.com>
2021-04-25 16:28:53 +0300

    Fix the auto-generation of ETH documents (currently for v0.6.36 only).

--- generate-eth-docs.js
+++ generate-eth-docs.js
@@ -1,49 +1,42 @@
-require("download-git-repo")("bancorprotocol/contracts-solidity", "node_modules/@bancor/contracts-solidity", (error) => {
-    if (error) {
-        throw error;
+const fs = require("fs");
+const path = require("path");
+const spawnSync = require("child_process").spawnSync;
+
+const args = [
+    "node_modules/solidity-docgen/dist/cli.js",
+    "--input=node_modules/@bancor/contracts-solidity/solidity/contracts",
+    "--output=ethereum-contracts/ethereum-api-reference",
+    "--templates=config/ethereum-smart-contracts",
+    "--solc-module=solc",
+    "--solc-settings=" + JSON.stringify({ optimizer: { enabled: true, runs: 200 }})
+];
+
+const result = spawnSync("node", args, { stdio: ["inherit", "inherit", "pipe"], encoding: 'utf8' });
+if (result.stderr.length > 0) {
+    throw new Error(result.stderr);
+}
+
+function fix(pathName) {
+    if (fs.lstatSync(pathName).isDirectory()) {
+        for (const fileName of fs.readdirSync(pathName))
+            fix(path.join(pathName, fileName));
     }
-
-    const fs = require("fs");
-    const path = require("path");
-    const spawnSync = require("child_process").spawnSync;
-
-    const args = [
-        "node_modules/solidity-docgen/dist/cli.js",
-        "--input=node_modules/@bancor/contracts-solidity/solidity/contracts",
-        "--output=ethereum-contracts/ethereum-api-reference",
-        "--templates=config/ethereum-smart-contracts",
-        "--solc-module=solc",
-        "--solc-settings=" + JSON.stringify({ optimizer: { enabled: true, runs: 200 }})
-    ];
-
-    const result = spawnSync("node", args, { stdio: ["inherit", "inherit", "pipe"], encoding: 'utf8' });
-    if (result.stderr.length > 0) {
-        throw new Error(result.stderr);
-    }
-
-    function fix(pathName) {
-        if (fs.lstatSync(pathName).isDirectory()) {
-            for (const fileName of fs.readdirSync(pathName))
-                fix(path.join(pathName, fileName));
+    else if (pathName.endsWith(".md")) {
+        const lines = fs.readFileSync(pathName, { encoding: "utf8" }).split("\r").join("").split("\n");
+        fs.unlinkSync(pathName)
+        
+        let newPath = pathName.toLowerCase();
+        if (newPath.indexOf(path.join('converter', 'types')) != -1) {
+            const parts = newPath.split(path.sep);
+            parts.splice(parts.length - 2, 1);
+            newPath = parts.join(path.sep);
         }
-        else if (pathName.endsWith(".md")) {
-            const lines = fs.readFileSync(pathName, { encoding: "utf8" }).split("\r").join("").split("\n");
-            fs.unlinkSync(pathName)
-            
-            let newPath = pathName.toLowerCase();
-            if (newPath.indexOf(path.join('converter', 'types')) != -1) {
-                const parts = newPath.split(path.sep);
-                parts.splice(parts.length - 2, 1);
-                newPath = parts.join(path.sep);
-            }
 
-            fs.writeFileSync(newPath, lines.filter(line => line.trim().length > 0).join("\n\n") + "\n");
-        }
+        fs.writeFileSync(newPath, lines.filter(line => line.trim().length > 0).join("\n\n") + "\n");
     }
+}
 
-    fix("ethereum-contracts/ethereum-api-reference");
+fix("ethereum-contracts/ethereum-api-reference");
 
-    fs.copyFileSync("node_modules/@bancor/contracts-solidity/README.md", "ethereum-contracts/ethereum-api-reference/README.md");
-    //fs.copyFileSync("node_modules/bancor-contracts/solidity/utils/README.md", "ethereum-contracts/ethereum-api-reference/utils/README.md");
-    //fs.copyFileSync("node_modules/bancor-contracts/solidity/python/README.md", "ethereum-contracts/ethereum-api-reference/python/README.md");
-});
+fs.copyFileSync("node_modules/@bancor/contracts-solidity/README.md", "ethereum-contracts/ethereum-api-reference/README.md");
+//fs.copyFileSync("node_modules/bancor-contracts/solidity/utils/README.md", "ethereum-contracts/ethereum-api-reference/utils/README.md");
